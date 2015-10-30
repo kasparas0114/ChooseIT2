31e6d90d252a1701030da0ffeb8c3895eabc831d
kasparas0114 <kasparas.stadalnikas@gmail.com>
2015-10-30 13:42:05 +0200

    Initial test commit

---
+++ app/src/main/java/com/example/kasparas/chooseit/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.kasparas.chooseit;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView photo;
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        photo= (ImageView) findViewById(R.id.imageView);
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                //Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                //startActivity(i);
+                setContentView(R.layout.fragment_selectoptions);
+                // close this activity
+                //finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+
+   /* @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }*/
+    //@Override
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void onLoadedApp () {
+
+    }
+}
